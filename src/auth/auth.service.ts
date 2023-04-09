869a72a8564d1cae5c2739a994478d77733e5ccc
Alexander <aleksievisa@gmail.com>
2023-04-10 01:56:23 +0300

    feat: add register method

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
+import { AuthDto } from './auth.dto';
+import { faker } from '@faker-js/faker';
+import { hash } from 'argon2';
 
 @Injectable()
 export class AuthService {
-  register() {
-    return { name: 'sds' };
+  constructor(private prisma: PrismaService) {}
+
+  async register(dto: AuthDto) {
+    const oldUser = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+
+    if (oldUser) throw new BadRequestException('User already exists');
+
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        name: faker.name.firstName(),
+        avatarPath: faker.image.avatar(),
+        phone: faker.phone.number('+375 (##-###-##-##)'),
+        password: await hash(dto.password),
+      },
+    });
+
+    return user;
   }
 }
