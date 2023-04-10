8643dc359580adf536735dd5e14821e96453ef0e
Alexander <aleksievisa@gmail.com>
2023-04-10 16:10:49 +0300

    feat: add category module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,12 @@ import { AuthModule } from './auth/auth.module';
 import { PrismaService } from './prisma.service';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
+import { ProductModule } from './product/product.module';
+import { CategoryModule } from './category/category.module';
+import { CategoryModule } from './category/category.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), AuthModule, UserModule],
+  imports: [ConfigModule.forRoot(), AuthModule, UserModule, ProductModule, CategoryModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
