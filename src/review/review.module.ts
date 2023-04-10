ae0902e0d5810fb259448a8034b2ccf1b0920531
Alexander <aleksievisa@gmail.com>
2023-04-10 19:06:22 +0300

    feat: add statistics service

--- src/review/review.module.ts
+++ src/review/review.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ReviewService } from './review.service';
 import { ReviewController } from './review.controller';
 import { PrismaService } from 'src/prisma.service';
+import { UserService } from 'src/user/user.service';
 
 @Module({
   controllers: [ReviewController],
-  providers: [ReviewService, PrismaService],
+  providers: [ReviewService, PrismaService, UserService],
 })
 export class ReviewModule {}
